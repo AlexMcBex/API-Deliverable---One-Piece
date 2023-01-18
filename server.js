d8f6f3c80184f8fb33105a6604c17317c2e66af8
AlexMcBex <alessiobelli94@gmail.com>
2023-01-17 21:01:31 -0500

    INDEX route made

--- server.js
+++ server.js
@@ -42,7 +42,40 @@ app.use(express.json())
 app.get("/", (req, res)=>{
     res.send('<h1>HOME PAGE</h1>')
 })
+    //SEED
+    app.get('/mugiwara/seed', (req, res) => {
+        // array of starter resources(fruits)
+        const startCrew = [
+            { name: "Monkey D Rufy", rank: "Captain", devilFruit: true, reward: 3000000000,},
+            { name: "Roronoa Zoro", rank: "Vice-Captain", devilFruit: false, reward: 1111000000,},
+            { name: "Nami", rank: "Navigator", devilFruit: false, reward: 366000000,},
+            { name: "Usopp", rank: "Sniper", devilFruit: false, reward: 500000000,},
+            { name: "Sanji", rank: "Cook", devilFruit: false, reward: 1032000000,},
+            { name: "Chopper", rank: "Doctor", devilFruit: true, reward: 1000,},
+            { name: "Nico Robin", rank: "Scholar", devilFruit: true, reward: 930000000,},
+            { name: "Franky", rank: "Shipwright", devilFruit: false, reward: 394000000,},
+            { name: "Brook", rank: "Musician", devilFruit: true, reward: 383000000,},
+        ]
+        Mugiwara.deleteMany({})
+        .then(() => {
+            Mugiwara.create(startCrew)
+                .then(data => {
+                    res.json(data)
+                })
+                .catch(err => console.log('The following error occurred: \n', err))
+        })
+})
     //CRUD Routes
+        //INDEX
+app.get("/mugiwara", (req, res)=>{
+    Mugiwara.find({})
+        .then(pirate =>{res.json({pirate:pirate})})
+        .catch(err => console.log('The following error occurred: \n', err))
+})
+        //SHOW
+        //POST
+        //PATCH
+        //DELETE
 
 //7. SERVER LISTENER
     //const PORT, app.listen
